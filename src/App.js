4401b1622c41cd4a189af6751078e201db4ba1fb
Srujana <1nt23ec156.srujana@nmit.ac.in>
2025-09-06 14:44:57 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import CategoryPredictor from "./components/CategoryPredictor";
+
+function App() {
+  return (
+    <div>
+      <CategoryPredictor />
+    </div>
+  );
+}
+
+export default App;
